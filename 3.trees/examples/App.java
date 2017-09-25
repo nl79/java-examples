16ab5954bf9157754c7c5305704dfbe8a52646f4
Nazar Lesiv <nazarlesiv@Nazars-MBP-2.home>
2017-09-24 20:20:07 -0400

    Initial character reader

--- 3.trees/examples/App.java
+++ 3.trees/examples/App.java
@@ -11,6 +11,8 @@ public class App {
     Scanner reader = new Scanner(System.in);
     System.out.print("Enter Expression: ");
     expression = reader.nextLine();
-    
+
+    System.out.print("CharAt(0): " + (int)expression.charAt(0));
+    System.out.print("CharAt(0) == ' ': " + (expression.charAt(0) == ' '));
   }
 }
--- 3.trees/examples/Expression.java
+++ 3.trees/examples/Expression.java
@@ -1,3 +1,54 @@
+class Pipe {
+  String s;
+  int pos = 0;
+
+  boolean ignoreEmpty = true;
+
+  public Pipe(String s) {
+    this.s = s;
+  }
+
+  public boolean hasNext() {
+    return (this.pos + 1) < this.s.length();
+  }
+
+  // Get the next token.
+  public char next() throws Exception {
+    return (char)this.next(1);
+
+  }
+
+  // Get the next token at position i.
+  public char next(int i) throws Exception {
+
+    if(this.hasNext()) {
+      this.pos = this.pos + 1;
+      return this.s.charAt(pos);
+    } else {
+      throw new Exception("End of input reached");
+    }
+  }
+
+  // Look ahead 1 token.
+  public char peak() {
+    return this.peak(1);
+  }
+
+  // Look ahead n tokens.
+  public char peak(int n) {
+    int pos = this.pos + n;
+    if(pos < this.s.length()) {
+      return this.s.charAt(pos);
+    } else {
+      return '\0';
+    }
+  }
+
+  public void ignoreEmpty(boolean b) {
+    this.ignoreEmpty = b;
+  }
+}
+
 class Node {
   public Node() {
 
@@ -12,40 +63,6 @@ class Node {
     // Read the next token.
     int pos = exp.indexOf(" ");
     String sub = exp.substring(0, pos);
-  }
-
-}
-
-class Digit extends Node {
-
-  double val;
-
-  public Digit(double val) {
-    this.val = val;
-  }
-}
-
-class Term extends Node {
-  public Term() {
-
-  }
-}
-
-class Operator extends Node {
-  String op;
-
-  public Operator(String op) {
-    this.op = op;
-  }
-}
-
-public class Expression extends Node {
-  String exp;
-
-  public Expression(String exp) {
-    this.exp = exp;
-
-    this.parse(this.exp);
   }
 
 }
